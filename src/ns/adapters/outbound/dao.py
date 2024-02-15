a0e63236db46ebb70e5362d9f05947f253e77bee
TheByronHimes <TheByronHimes@gmail.com>
2024-02-15 16:13:13 +0000

    Add DAO for notification records

---
+++ src/ns/adapters/outbound/dao.py
@@ -0,0 +1,33 @@
+# Copyright 2021 - 2023 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""DAO translator constructor"""
+
+from hexkit.protocols.dao import DaoFactoryProtocol
+
+from ns.core import models
+from ns.ports.outbound.dao import NotificationRecordDaoPort
+
+
+async def notification_record_dao_factory(
+    *, dao_factory: DaoFactoryProtocol
+) -> NotificationRecordDaoPort:
+    """Construct a NotificationRecordDaoPort from the provided dao_factory"""
+    return await dao_factory.get_dao(
+        name="notification_records",
+        dto_model=models.NotificationRecord,
+        id_field="hash_sum",
+    )
